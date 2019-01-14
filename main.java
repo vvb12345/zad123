3f11d80c16719c712f2067f7e5b08868d1e2c7c9
vvb12345 <46691387+vvb12345@users.noreply.github.com>
2019-01-15 00:08:29 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+﻿public class Main {
+   public static void main(String[] args) {
+
+
+System.out.println("Hello world"); System.out.println(isPalindrome("машина")); System.out.println(isPalindrome(“шалаш"));
+
+
+
+
+       for (int i = 0; i < 100; i++) {
+       if (isPrime(i))
+       System.out.println(i);
+       }
+   }
+
+
+public static boolean isPalindrome(String string) {
+       return new StringBuilder(string).reverse().toString().equals(string);
+   }
+   public static boolean isPrime(final int num) {
+   return num > 1 && IntStream.range(2, num).noneMatch(index -> num % index == 0);
+   }
+}
